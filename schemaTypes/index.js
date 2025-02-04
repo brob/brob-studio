3ca08f42eb7e1d22f0d81b99d501205505f775ff
brob <bryanlrobinson@gmail.com>
2025-02-04 09:57:34 -0500

    Adds bookmarks

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,4 +1,40 @@
+
 export const schemaTypes = [
+    {  
+        name: 'bookmark',
+        title: 'Bookmarks',
+        type: 'document',
+        fields: [
+          {
+            name: 'pageTitle',
+            title: 'Title',
+            type: 'string'
+          },
+          {
+            name: 'url',
+            title: 'URL',
+            type: 'string'
+          },
+          {
+            name: 'description',
+            title: 'Description',
+            type: 'text'
+          },
+          {
+            title: 'Save Date',
+            name: 'time',
+            type: 'datetime',
+            options: {
+              dateFormat: 'YYYY-MM-DD',
+              timeFormat: 'HH:mm',
+              timeStep: '15',
+              calendarTodayLabel: 'today',
+            },
+          },
+          
+        ]
+      },
+    
     {
         name: 'blog',
         type: 'document',
@@ -47,6 +83,13 @@ export const schemaTypes = [
                 type: 'datetime',
                 title: 'Published at',
                 validation: Rule => Rule.required()
+            },
+            {
+                name: 'canonical',
+                type: 'url',
+                title: 'Canonical URL',
+                validation: Rule => Rule.uri()
+                
             },
             {
                 name: 'body',
