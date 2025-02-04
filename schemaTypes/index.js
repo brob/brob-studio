45e69a301a5a68a1b87a3bb43534d94c9ac1cee0
brob <bryanlrobinson@gmail.com>
2025-02-04 09:58:57 -0500

    Adds newsletter model

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,5 +1,37 @@
 
 export const schemaTypes = [
+    {
+        name: 'newsletter',
+        title: 'Newsletter',
+        type: 'document',
+        fields: [
+            {
+                name: 'title',
+                title: 'Subject Line',
+                type: 'string'
+            },
+            {
+                title: 'Opening',
+                name: 'opening',
+                type: 'array',
+                of: [{type: 'block'}]
+            },
+            {
+                title: 'Bookmarks',
+                name: 'bookmarks',
+                type: 'array',
+                of: [
+                    {
+                        type: 'reference',
+                        to: [
+                            {type: 'bookmark'},
+                        ],
+                    },
+                    
+                ],
+            },
+        ]
+    },
     {  
         name: 'bookmark',
         title: 'Bookmarks',
